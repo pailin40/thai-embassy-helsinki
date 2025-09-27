155fe794820b6510bc75e8f7563157e07321035d
pailin <147715559+pailin40@users.noreply.github.com>
2025-09-27 11:34:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/thai-embassy-helsinki/',  // This fixes the blank page!
+  base: '/thai-embassy-helsinki/',
   build: {
     outDir: 'dist',
   },
